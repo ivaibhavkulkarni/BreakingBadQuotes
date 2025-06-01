53ff84dddac39dfce92bb957e5f14aa7fd500f8b
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-01 16:50:55 +0530

    displaying quotes, images, char name

--- BBQuotes/QuoteView.swift
+++ BBQuotes/QuoteView.swift
@@ -8,21 +8,51 @@
 import SwiftUI
 
 struct QuoteView: View {
+    let vm = ViewModel()
+    let show: String
+    
     var body: some View {
-        TabView{
-            Tab("Breaking Bad", systemImage: "tortoise"){
-                Text("Breaking Bad")
-                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+        GeometryReader { geo in
+            ZStack {
+                Image(show.lowercased().replacingOccurrences(of: " ", with: " "))
+                    .resizable()
+                    .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+                VStack{
+                    Text("\"\(vm.quote.quote)\"")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.black.opacity(0.5))
+                        .clipShape(.rect(cornerRadius: 25))
+                        .padding(.horizontal)
+                    ZStack(alignment: .bottom){
+                        AsyncImage(url: vm.character.images[0]){ image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        }placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                        
+                        Text(vm.quote.character)
+                            .foregroundStyle(.white)
+                            .padding(10)
+                            .frame(maxWidth: .infinity)
+                            .background(.ultraThinMaterial)
+                    }
+                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    .clipShape(.rect(cornerRadius: 50))
+                }
+                .frame(width: geo.size.width)
             }
-            Tab("Better Call Saul", systemImage: "briefcase"){
-                Text("Better Call Saul")
-                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
-            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .preferredColorScheme(.dark)
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    QuoteView()
+    QuoteView(show: "breakingbad")
+        .preferredColorScheme(.dark)
 }
