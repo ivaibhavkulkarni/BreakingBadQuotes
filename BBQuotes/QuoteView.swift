d94d7e71f0ae64bbd65986836091cbff48cba70a
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-07 12:36:08 +0530

    Fixed Button

--- BBQuotes/QuoteView.swift
+++ BBQuotes/QuoteView.swift
@@ -18,37 +18,48 @@ struct QuoteView: View {
                     .resizable()
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 VStack{
-                    Spacer(minLength: 60)
-                    Text("\"\(vm.quote.quote)\"")
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.white)
-                        .padding()
-                        .background(.black.opacity(0.5))
-                        .clipShape(.rect(cornerRadius: 25))
-                        .padding(.horizontal)
-                    
-                    ZStack(alignment: .bottom){
-                        AsyncImage(url: vm.character.images[0]){ image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        }placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                    VStack{
+                        Spacer(minLength: 60)
                         
-                        Text(vm.quote.character)
-                            .foregroundStyle(.white)
-                            .padding(10)
-                            .frame(maxWidth: .infinity)
-                            .background(.ultraThinMaterial)
+                        switch vm.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text("\"\(vm.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom){
+                                AsyncImage(url: vm.character.images[0]){ image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                }placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                                
+                                Text(vm.quote.character)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                            }
+                            .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case .failed(let error):
+                            Text(error.localizedDescription)
+                        }
+                        
+                        Spacer()
                     }
-                    .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
-                    .clipShape(.rect(cornerRadius: 50))
-                    
-                    Spacer()
-                    
                     Button{
                         Task{
                             await vm.getData(for: show)
@@ -58,9 +69,9 @@ struct QuoteView: View {
                         .font(.title3)
                         .foregroundStyle(.white)
                         .padding()
-                        .background(.breakingBad)
+                        .background(Color("\(show.replacingOccurrences(of: " ", with: ""))Button"))
                         .clipShape(.rect(cornerRadius: 15))
-                        .shadow(color: .breakingBadYellow, radius: 4)
+                        .shadow(color:Color("\(show.replacingOccurrences(of: " ", with: ""))Shadow"), radius: 2)
                     }
                     Spacer(minLength: 95)
                 }
